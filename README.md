2481636f02fd48b5dde0270a8c092f9c897af4f8
T-Jay Tipps <deansredhalo@gmail.com>
2015-03-23 17:07:01 -0500

    readme update

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 Tritt is a lightweight, fast, simple-to-use tool for creating inline custom elements complete with Shadow DOM, external style/script imports, encapsulation and of course simple event and data binding.  It allows you to create a web component spec custom element that can be used throughout your page. Tritt is built on top of Google's [webcomponentsjs](https://www.github.com/webcomponents/webcomponentsjs) polyfills and uses [Gulp](http://gulpjs.com) for building its imports.
 
-Like all of my current projects, it's named after a 90s era country star, this one of course is named after none other than Travis Tritt.
+Like all of my current projects, it's named after a 90s era country star, this one of course none other than Travis Tritt.
 
 ## Installation
 
@@ -25,6 +25,139 @@ You're ready to roll at this point.
 
 ## Usage
 
+Tritt makes it very simple to create custom elements with bindings and custom events attached. Either create a new JS file, or inline in the HEAD of your HTML file inside a `<script>` tag, you can invoke Tritt to run on the custom element you want to create.
+
+### Invoke Tritt (JS)
+
+```js
+Tritt('element-name', {actions});
+```
+
+The element name is required, however the second parameter (the actions) is not.  If you just want to register your custom element with no data binding to any special events, that is all you will need.  
+
+To invoke Tritt with actions, note the following example
+
+```js
+Tritt('element-name', {
+	events: {
+		myEvent: function(rootElement, rootShadow, bindings) {
+			...
+		}
+	},
+	bindings: {
+		myBinding: "Your name here"
+	}
+});
+```
+### HTML Markup for Tritt (HTML)
+
+Create your element as you would any other piece of HTML markup.  It must contain a `-` in the name.
+
+```html
+<element-name>
+</element-name>
+```
+
+You can place content inside your element just as you would any other HTML element, such as
+
+```html
+<element-name>
+	<div>
+		<span>Here's some text</span>
+	</div>
+</element-name>
+```
+
+To have Tritt create a shadow DOM for you in your element, you can use the **shadow** attribute on your element.
+
+```html
+<element-name shadow>
+	...
+</element-name>
+```
+
+Any content found inside the element will automatically be moved into the shadow DOM.
+
+If you'd like to include an external script or style within your element, you can use the **script** and **style** attributes to specify your local files. **Note:** Tritt expects a shadow DOM within your element to include external scripts and styles.
+
+```html
+<element-name shadow script="script.js" style="style.css">
+	...
+</element-name>
+```
+
+Tritt will parse these files and place their contents at the beginning and end of the shadow DOM.
+
+### Data and event binding in Tritt
+
+Tritt employs a simple yet poweful data and event binding system.  It uses familiar {{}} syntax to denote bindings, the main difference being between text bindings and event bindings. These events should be specified in your actions object when you invoked Tritt.
+
+Text binding
+	{{text}}
+
+Event binding
+	{{event()}}
+
+Note the inclusion of the () at the end of the event name.  Text bindings are bound to an element using the **bind** attributeEvents are also bound to the element they should affect by using the the **action** attribute, such as
+
+```html
+<element-name shadow script="script.js" style="style.css" action="{{event()}}">
+	<span bind="{{text}}"></span>
+</element-name>
+```
+
+You can have multiple events bound to one element, just separate each of them with a space.
+
+Event bindings have access to the text bindings listed under the `bindings` object of the actions.  Example
+
+```js
+Tritt('element-name', {
+	events: {
+		myEvent: function(rootElement, rootShadow, bindings) {
+			bindings.myBinding: "New name goes here"
+		}
+	},
+	bindings: {
+		myBinding: "Your name here"
+	}
+});
+```
+
+Tritt's events system allows you to write normal JS commands in the event and using the **rootElement** and **rootShadow** parameters, you can target the specific element you have bound the event to. Thus, clicks and mouseovers can be used and will only affect your custom element, although global access and access to other elements is available to you.
+
+Tritt will automatically update bindings for you within your custom element when they change.
+
+### Putting it all together
+
+Here's an example of all the Tritt functionality we've covered:
+
+```js
+Tritt('custom-greeting', {
+	events: {
+		changeGreeting: function(rootElement, rootShadow, bindings) {
+			rootElement.addEventListener('mouseover', function() {
+				bindings.userName = 'Travis Tritt!'
+			});
+		}
+	},
+	bindings: {
+		userName: "Reveal user name..."
+	}
+});
+```
+
+```html
+<custom-greeting shadow style="style.css">
+	<div action="{{changeGreeting()}}">
+		<span bind="{{userName}}"></span>
+	</div>
+</custom-greeting>
+``` 
+
 ## Things to do
+
+Thorough testing and unit tests.
+
+Add `light` attribute support.
 
 Add Gulp task to export custom element for HTML Imports.
--- tritt.js
+++ tritt.js
@@ -12,25 +12,59 @@
 *
 *****************************************/
 
+
+/**
+ * Global variable for all items that we create.
+ *
+ * @global {array} window.hostElements
+ */
 window.hostElements = [];
 
+
+/*
+ * Main constructor function
+ *
+ * @function Tritt
+ * @param {string} element The name of the element to create.
+ * @param {object} events The events and bindings attached to the element.
+ */
 window.Tritt = function(element, events) {
 	var that = this;
 
+	// error catch
+	if (element === undefined) {
+		throw 'No element name!';
+	}
+
+	// if the events are undefined we want to create them 
+	// as an object anyway
 	events = events || {};
 
+	// call addHostElements
 	Tritt.addHostElements(element, events);
 
+	// this will allow Tritt to make sure all the DOM elements have been
+	// successfully loaded before we try and begin
 	window.addEventListener('DOMContentLoaded', function() {
+
+		// call the primary functions of Tritt, in order
 		Tritt.registerElement(element, events);
 		Tritt.createShadowDOM(element);
 		Tritt.parseStylesAndScripts(element);
 		Tritt.parseBindings(element, events['bindings']);
+
 	}, false);
 };
 
+
+/*
+ * Add our elements to the global hostElements variable
+ *
+ * @function addHostElements
+ * @param {string} element The name of the element we created
+ * @param {object} events The events and bindings attached to the element
+ */
 Tritt.addHostElements = function(element, events) {
-	console.log(events);
 	window.hostElements.push({ 'name': element });
 	for (var i = 0; i < window.hostElements.length; i++) {
 		if (window.hostElements[i]['name'] === element) {
@@ -39,21 +73,32 @@ Tritt.addHostElements = function(element, events) {
 	}
 }
 
+
+/*
+ * Register our custom element with a custom prototype
+ *
+ * @function registerElement
+ * @param {string} element The name of the element we created
+ * @param {object} events The events and bindings attached to the element
+ */
 Tritt.registerElement = function(element, events) {
 	var that = this;
 
 	var CustomElementPrototype;
 	var custom_element;
 
+	// new basic prototype
 	CustomElementPrototype = Object.create(HTMLElement.prototype);
 
+	// let's try and register the element...
 	try {
+		// register the element with our prototype
 		custom_element = document.registerElement(element, {
 			prototype: CustomElementPrototype
 		});
 
+		// attach the events
 		Tritt.attachEvents(element, events).then(function(response) {
-			console.log(element);
 			custom_element = response;
 		});
 
@@ -61,22 +106,36 @@ Tritt.registerElement = function(element, events) {
 
 		return;
 	}
+	// ...if we can't, or it's already been registered, just ignore it and move on
 	catch (err) {
 		return;
 	}
 };
 
+
+/*
+ * Create the shadow DOM for our elements if specified
+ *
+ * @function createShadowDOM
+ * @param {string} element The name of the element we created
+ */
 Tritt.createShadowDOM = function(element) {
 	var that = this;
 
 	var unaltered;
 
+	// let's get the element before we do any modifications to it
 	unaltered = document.querySelectorAll(element);
 
+	// self-executing function to check all of our elements
+	// that use this name
 	(function() {
 		for (var i = 0; i < unaltered.length; i++) {
+			// does it have attributes?
 			if (unaltered[i].hasAttributes()) {
+				// ...does it have a shadow attribute?
 				if (unaltered[i].attributes['shadow'] !== undefined) {
+					// populate the shadow DOM with content
 					window.Tritt.populateShadowDOM(unaltered[i]);
 				}
 			}
@@ -84,11 +143,21 @@ Tritt.createShadowDOM = function(element) {
 	})();
 };
 
-Tritt.populateShadowDOM = function(element) {	
+
+/*
+ * Move content into the Shadow DOM
+ *
+ * @function populateShadowDOM
+ * @param {string} element The name of the element we created
+ */
+Tritt.populateShadowDOM = function(element) {
+	// create a shadow DOM portion	
 	var shadow = element.createShadowRoot();
 
+	// fill it with the HTML content of the element
 	shadow.innerHTML = element.innerHTML;
 
+	// remove the light DOM content
 	element.innerHTML = null;
 
 	console.log('Created and populated Shadow DOM for element', element);
@@ -96,6 +165,13 @@ Tritt.populateShadowDOM = function(element) {
 	return;
 };
 
+
+/*
+ * Dynamically include the specified scripts and styles
+ *
+ * @function parseStylesAndScripts
+ * @param {string} element The name of the element we created
+ */
 Tritt.parseStylesAndScripts = function(element) {
 	var that = this;
 
@@ -107,41 +183,61 @@ Tritt.parseStylesAndScripts = function(element) {
 	var scriptNodeTypeAttr;
 	var scriptContents;
 
-
+	// does our element have attributes?
 	if (element.hasAttributes()) {
+		// does our element have a shadow DOM?
 		if (element.attributes['shadow'] !== undefined) {
 
+			// does our element have a style attribute
 			if (element.attributes['style'].nodeValue !== undefined) {
+
+				// create a style element
 				styleNode = document.createElement('style');
 
+				// set the type attribute to text/css
 				styleNodeTypeAttr = document.createAttribute('type');
 				styleNodeTypeAttr.value = "text/css";
 
+				// read the style file
 				Tritt.processFile(element.attributes['style'].nodeValue).then(function(response) {
+
+					// set the content
 					styleContents = response;
 
+					// add the type attribute
 					styleNode.attributes.setNamedItem(styleNodeTypeAttr);
 
+					// make the content equal the contents of the file
 					styleNode.textContent = styleContents;
 
+					// place this guy before the first element in the shadow DOM
 					element.shadowRoot.insertBefore(styleNode, element.shadowRoot.childNodes[0]);
-
 				});
 			}
 
+			// what about a script attribute?
 			if (element.attributes['script'].nodeValue !== undefined) {
+
+				// create a new element for the script
 				scriptNode = document.createElement('script');
 
+				// set the script type attribute to text/javascript
 				scriptNodeTypeAttr = document.createAttribute('type');
 				scriptNodeTypeAttr.value = "text/javascript";
 
+				// read the script
 				Tritt.processFile(element.attributes['script'].nodeValue).then(function(response) {
+					
+					// grab the contents
 					scriptContents = response;
 
+					// add the type attribute
 					scriptNode.attributes.setNamedItem(scriptNodeTypeAttr);
 
+					// set the content equal to the file
 					scriptNode.textContent = scriptContents;
 					
+					// insert the new tag before the last element in the shadow DOM
 					element.shadowRoot.insertBefore(scriptNode, element.shadowRoot.childNodes[element.shadowRoot.childNodes.length]);
 				});
 			}
@@ -149,31 +245,55 @@ Tritt.parseStylesAndScripts = function(element) {
 	}
 };
 
+/*
+ * Read our file
+ *
+ * @function processFile
+ * @param {string} file Filename to go and get
+ * @returns {string} response The responseText of the file
+ */
 Tritt.processFile = function(file) {
-	var loc 		= window.location.pathname;
-	var dir 		= loc.substring(0, loc.lastIndexOf('/'));
+	// var loc 		= window.location.pathname;
+	// var dir 		= loc.substring(0, loc.lastIndexOf('/'));
 	var xmlhttp;
 
-	file = 'http://' + window.location.host + dir + '/' + file;
+	// set the full pathname for the reader
+	// file = 'http://' + window.location.host + dir + '/' + file;
 
+	// create a new Promise object
 	return new Promise(function(resolve, reject) {
+		// new XMLHttpRequest object
 		xmlhttp = new XMLHttpRequest();
+		// do a get
 		xmlhttp.open('GET', file, true);
+		// if everything goes smoothly, resolve with the contents of the file
 		xmlhttp.onreadystatechange = function() {
 			if (xmlhttp.status == 200 && xmlhttp.readyState == 4) {
 				resolve(this.responseText);
 			}
 		}
+		// send our request
 		xmlhttp.send();
 	});
 };
 
+
+/*
+ * Attach the events to the element
+ *
+ * @function attachEvents
+ * @param {string} element The name of the element we created
+ * @param {object} events The events and bindings attached to the element
+ */
 Tritt.attachEvents = function(element, events) {
 
+	// create a new Promise object
 	return new Promise(function(resolve, reject) {
 		element = document.querySelector(element);
 
+		// if we have events
 		if (events) {
+			// loop through and create keys
 			for (var key in events['events']) {
 				if (key !== undefined) {
 					element[key] = events.events[key];
@@ -183,6 +303,14 @@ Tritt.attachEvents = function(element, events) {
 	});
 };
 
+
+/*
+ * Add our elements to the global hostElements variable.
+ *
+ * @function parseBindings
+ * @param {string} element The name of the element we created
+ * @param {object} bindings The bindings attached to the element
+ */
 Tritt.parseBindings = function(element, bindings) {
 	var elementSel 		= document.querySelector(element);
 	var parseElement 	= elementSel.shadowRoot;
@@ -192,40 +320,65 @@ Tritt.parseBindings = function(element, bindings) {
 	var bindValue;
 	var rewritable;
 
+	// do we have a shadow DOM?
 	if (elementSel.shadowRoot) {
+		// look for all text between {{ }} and return those elements
 		bound = elementSel.shadowRoot.innerHTML.match(/{{(.*?)}}/g);
 
+		// loop them
 		for (var i = 0; i < bound.length; i++) {
 
+			// if we have the bindings markup
 			if (bound[i].indexOf('{{') !== -1) {
+				// remove the sets of braces	
 				bound1 = bound[i].replace(/{{/g, '');
 				bound2 = bound1.replace(/}}/g, '');
 
+				// if there's not a function marker, we know it's a text binding
 				if (bound2.indexOf('()') === -1) {
+					// make sure we have a bind attribute that it's assigned to
 					if (elementSel.shadowRoot.querySelectorAll('[bind]')) {
+						// loop through any of the elements that have that particular binding
 						for (var j = 0; j < elementSel.shadowRoot.querySelectorAll('[bind]').length; j++) {
+							// make sure the values match the element selected
 							if (elementSel.shadowRoot.querySelectorAll('[bind]')[j].attributes['bind'].nodeValue === bound[i]) {
+								// find the value of the binding
 								bindValue = bindings[bound2];
+								// attach it
 								elementSel.shadowRoot.querySelectorAll('[bind]')[j].textContent = bindValue;		
 							} 
 						}
 					}
 				}
+				// otherwise, we have a function binding
 				else {
+					// get rid of the parentheses
 					rewritable = bound2.replace(/\(\)/g, '');
+					// run the function assigned
 					elementSel[rewritable](elementSel, parseElement, bindings);
 				}
 			}
 		}
 	}
 
+	// observe the binding for any changes that may occur
 	Tritt.watchForChanges(element, bindings);
 };
 
+
+/*
+ * Watch any bindings objects for updates
+ *
+ * @function watchForChanges
+ * @param {string} element The name of the element we created
+ * @param {object} object The object we want to observe
+ */
 Tritt.watchForChanges = function(element, object) {
 	if (object !== undefined) {
+		// observe the object and note any changes
 		Object.observe(object, function(changes) {
 			for (var i = 0; i < changes.length; i++) {
+				// re-run the binding function with the updates
 				Tritt.parseBindings(element, changes[i].object);
 			}
 		});
